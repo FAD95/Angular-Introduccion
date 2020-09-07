e6d92ee7e28ee04b528f4f2ead575eabf1a9fe5a
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-06 23:15:59 -0500

    Agregando mas componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { MyFirstComponent } from "./components/my-first/my-first.component";
+import { ImagesComponent } from "./components/images/images.component";
 
 @NgModule({
-  declarations: [AppComponent, MyFirstComponent],
+  declarations: [AppComponent, MyFirstComponent, ImagesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/images/images.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-images",
+  templateUrl: "./images.component.html",
+  styleUrls: ["./images.component.css"],
+})
+export class ImagesComponent implements OnInit {
+  constructor() {
+    console.log("images");
+  }
+
+  ngOnInit() {}
+}
