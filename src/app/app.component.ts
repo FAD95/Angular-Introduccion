5670fdbfb1de610848667e8fc4d33dc64690dea6
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-07 02:33:17 -0500

    Simple CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Employee } from "./models/employee";
 
 @Component({
   selector: "app-root",
@@ -6,5 +7,44 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  public title = "IntroAngular";
+  constructor() {
+    if (!localStorage.getItem("employeeArray")) {
+      localStorage.setItem(
+        "employeeArray",
+        JSON.stringify([
+          { id: 1, name: "Fad", country: "COL" },
+          { id: 2, name: "Cami", country: "CHL" },
+          { id: 3, name: "Gerard", country: "USA" },
+        ])
+      );
+    }
+  }
+  employeeArray: Employee[] = JSON.parse(localStorage.getItem("employeeArray"));
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+      localStorage.setItem("employeeArray", JSON.stringify(this.employeeArray));
+    }
+    this.selectedEmployee = new Employee();
+    localStorage.setItem("employeeArray", JSON.stringify(this.employeeArray));
+  }
+
+  openToEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+    localStorage.setItem("employeeArray", JSON.stringify(this.employeeArray));
+  }
+
+  delete() {
+    if (confirm("Are you sure you want to delete this employee?")) {
+      this.employeeArray = this.employeeArray.filter(
+        (x) => x !== this.selectedEmployee
+      );
+      this.selectedEmployee = new Employee();
+      localStorage.setItem("employeeArray", JSON.stringify(this.employeeArray));
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { MyFirstComponent } from "./components/my-first/my-first.component";
 import { ImagesComponent } from "./components/images/images.component";
+import { LogoComponent } from "./components/logo/logo.component";
+import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
+import { DangerButtonComponent } from './components/danger-button/danger-button.component';
 
 @NgModule({
-  declarations: [AppComponent, MyFirstComponent, ImagesComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MyFirstComponent,
+    ImagesComponent,
+    LogoComponent,
+    PrimaryButtonComponent,
+    DangerButtonComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
