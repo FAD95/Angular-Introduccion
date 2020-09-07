21b27a43711fc040cd14929e8ba470b9240b6491
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-06 22:37:29 -0500

    Usando props del componente en el template del html

--- src/app/components/my-first/my-first.component.ts
+++ src/app/components/my-first/my-first.component.ts
@@ -3,12 +3,20 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-my-first",
   template: `
-    <h1>Hola mundo</h1>
-    <p>Este es mi primer componente con Angular</p>
+    <h1>{{ title }}</h1>
+    <p>{{ coment }}</p>
+    <p>{{ year }}</p>
   `,
 })
 export class MyFirstComponent {
+  public title: string;
+  public coment: string;
+  public year: Date;
+
   constructor() {
+    this.title = "Hola mundo";
+    this.coment = "Este es mi primer componente con Angular";
+    this.year = new Date();
     console.log("Componente my-first cargado");
   }
 }
